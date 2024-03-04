14a04e727725c45783f13f646e71dd15bab04ee7
Suraj <mouryasuraj6266444@gmail.com>
2024-03-04 22:23:04 +0530

    fixed the bug, added optional chaining in thumbnails url

--- src/hooks/useVideoCard.js
+++ src/hooks/useVideoCard.js
@@ -5,10 +5,10 @@ import calculateDuration from "../utils/calulateDuration";
 const useVideoCard = (video) => {
     const { snippet, statistics, id, contentDetails } = video;
     const { title, thumbnails, channelTitle } = snippet;
-    const thumbnailUrl = thumbnails.maxres.url
+    const thumbnailUrl = thumbnails.maxres?.url
     const formattedViews = calculateViews(statistics?.viewCount);
     const formattedDate = calculateDate(snippet?.publishedAt);
-    const {formattedDuration} = calculateDuration(contentDetails.duration)
+    const { formattedDuration } = calculateDuration(contentDetails.duration)
 
 
     return { title, thumbnailUrl, channelTitle, id, formattedDate, formattedViews, formattedDuration }

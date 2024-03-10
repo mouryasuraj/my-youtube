d2c3d0aeb541e315353e8d578a9add5df8298fae
Suraj <mouryasuraj6266444@gmail.com>
2024-03-10 19:27:00 +0530

    add s to http in YOUTBE SUGGESTION API

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -113,5 +113,5 @@ export const YOUTBE_API_URL = 'https://youtube.googleapis.com/youtube/v3/videos?
 export const YOUTUBE_SEARCH_VIDOES_API = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=namastejavascript&maxResults=25&key=" + import.meta.env.VITE_YOUTUBE_KEY
 
 
-export const YOUTUBE_SUGGESTION_API = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
+export const YOUTUBE_SUGGESTION_API = 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
 
